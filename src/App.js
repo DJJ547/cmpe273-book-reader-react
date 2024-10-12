4a8dc02924ccbedf80ab6e19e2f11ccbe251659b
DinaLiao <dinaliao225@gmail.com>
2024-10-11 23:05:21 -0700

    added login page with gmail auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // ... other imports
 import "./index.css";
+import "semantic-ui-css/semantic.min.css"; // Import Semantic UI styles
+import { Container } from "semantic-ui-react"; // Import Semantic UI React components
 
 //utilities
 
@@ -11,15 +13,15 @@ import PageLayout from "./components/pageLayout/Navbar";
 //pages
 import Test from "./pages/Test";
 // import Main from "./pages/Main";
-// import Login from "./pages/auth/Login";
+import Login from "./pages/auth/Login";
 // import Signup from "./pages/auth/Signup";
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/test" element={<Test />} />
-        {/* <Route path="/auth/login" element={<Login />} /> */}
+        {/* <Route path="/test" element={<Test />} /> */}
+        <Route path="/auth/login" element={<Login />} />
         {/* <Route path="/auth/signup" element={<Signup />} /> */}
         {/* <Route path="/" element={<Main />} /> */}
         {/* <Route path="/test" element={<Test />} /> */}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // If using Google OAuth
+import 'semantic-ui-css/semantic.min.css'; // Import Semantic UI CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
+    <Router>
+      {/* If using Google OAuth, wrap the App with GoogleOAuthProvider */}
+    <GoogleOAuthProvider clientId="606148332855-99f34fqns59clmeghqaqgdvje9jlpu4a.apps.googleusercontent.com">
     <App />
-  </Router>
+  </GoogleOAuthProvider>
+    </Router> 
+</React.StrictMode>
 );
