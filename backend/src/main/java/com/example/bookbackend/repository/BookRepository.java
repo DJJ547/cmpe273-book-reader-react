8c7e15f4b3f03d37a4b3615b69749bfe0a4c028b
Jiajian Liu <jiajianliu10@gmail.com>
2024-10-26 21:11:17 -0700

    remove bakend

--- backend/src/main/java/com/example/bookbackend/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookbackend.repository;
-
-import com.example.bookbackend.model.Book;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<Book, String> {
-    // Custom query methods can be added here
-}
